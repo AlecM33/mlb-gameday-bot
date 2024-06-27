991ddfc0b76c384b9bcb47e3bc346f823bbf39e4
Alec <leohfx@gmail.com>
2024-06-27 17:19:31 -0400

    fix schema

--- database/schema.sql
+++ database/schema.sql
@@ -2,6 +2,6 @@ CREATE TABLE gameday_subscribe_channels(
     guild_id character varying(64) NOT NULL,
     channel_id character varying(64) NOT NULL,
     scoring_plays_only BOOLEAN NOT NULL,
-    delay smallint NOT NULL
+    delay smallint NOT NULL,
     PRIMARY KEY (guild_id, channel_id)
 );
